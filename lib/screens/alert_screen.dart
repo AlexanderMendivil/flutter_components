b89cbe5b1c4d235bbd67b086a0d88192fac0c41b
Alexander Mendivil <alexanderima@outlook.es>
2023-01-29 12:21:49 -0700

    alerts

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,17 +1,39 @@
-import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
   
+  void displayDialog(BuildContext context){
+    showDialog( 
+      // barrierDismissible: true, 
+      context: context, 
+      builder: ( context ) {return  AlertDialog(
+      elevation: 5,
+      title: const Text('titulo'),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      content: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: const <Widget>[
+        Text('Este es el contenido'),
+        SizedBox(height: 10,),
+        FlutterLogo(size: 100,),
+            ],
+          ),
+          actions: [
+            TextButton(onPressed: ()=> Navigator.pop(context), child: const Text('Cancelar'))
+          ],
+        );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Center(
          child: ElevatedButton(
-          onPressed: (){          
-         }, 
+          onPressed: ()=> displayDialog( context ),
+ 
          child: const Padding(
            padding: EdgeInsets.all(20),
            child: Text('Mostrar alerta', style: TextStyle(fontSize: 15 ),),
@@ -19,9 +41,7 @@ class AlertScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         elevation: 5,
-        onPressed: (){
-          Navigator.pop(context);
-      },
+        onPressed: ()=> Navigator.pop(context),
         child: const Icon(Icons.close)),
     );
   }
